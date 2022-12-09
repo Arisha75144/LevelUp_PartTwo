99226c51a488f176abb95245c4dd518c8ad6a664
Arina Morozova <ehdvevd@gmail.com>
2022-12-10 00:40:04 +0300

    tasks

---
+++ Week_2/String.cpp
@@ -0,0 +1,80 @@
+//
+// Created by Морозова Арина on 09.12.2022.
+//
+
+#include "String.hpp"
+
+
+String::String(){
+    _str= nullptr;
+}
+
+String::String(const char *str) {
+    _length= strlen(str);
+    str = new char [_length+1];
+    for (int i=0;i<_length;i++){
+        _str[i]=str[i];
+    }
+    _str[_length]='/0';
+
+}
+
+String::String(const String &str) {
+    _length= strlen(str._str);
+    this->_str = new char [_length+1];
+    for (int i=0;i<_length;i++){
+       this->_str[i]=str._str[i];
+    }
+   this-> _str[_length]='/0';
+}
+
+
+String::String(String &&str) noexcept {
+    this->_length=str._length;
+    this->_str=str._str;
+    str._length = 0;
+    str._str = nullptr;
+}
+String::~String() {
+
+    delete[] _str;
+}
+
+char &String::operator[](int index) {
+
+    return _str[index];
+
+}
+String String::operator+(const String &str) {
+    String Newstr;
+    int thisLength = strlen(this->_str);
+    int otherLength = strlen(str._str);
+
+    Newstr._length = thisLength + otherLength;
+
+    Newstr._str = new char[thisLength + otherLength + 1];
+    int i=0;
+    for (i; i < thisLength; i++)
+    {
+        Newstr._str[i] = this->_str[i];
+    }
+
+    for (int j = 0; j < otherLength; j++,i++)
+    {
+        Newstr._str[i] = str._str[j];
+    }
+
+    Newstr._str[thisLength + otherLength] = '\0';
+    return Newstr;
+
+
+
+}
+
+
+
+
+
+
+
+
---
+++ Week_2/String.hpp
@@ -0,0 +1,57 @@
+//
+// Created by Морозова Арина on 09.12.2022.
+//
+
+#ifndef LEVELUP_PARTTWO_STRING_HPP
+#define LEVELUP_PARTTWO_STRING_HPP
+
+#include <iostream>
+#include<compare>
+class String {
+
+public:
+    String();
+
+    String(const char *str);
+
+    String(const String &str);
+
+    String(String &&str) noexcept;
+
+    ~String();
+
+    [[nodiscard]] inline size_t length() const { return _length; }
+
+    [[nodiscard]] inline bool empty() const {
+        return _length == 0;
+    }
+
+
+
+
+    char &operator[](int index);
+
+
+    void append(const String &str);
+
+    void append(const char *str);
+
+
+
+     String operator+(const String& str);
+
+    friend std::ostream &operator<<(std::ostream &os, const String &str) {
+
+        os << str._str;
+        return os;
+    };
+
+
+
+private:
+    char *_str;
+    size_t _length;
+
+};
+
+#endif
